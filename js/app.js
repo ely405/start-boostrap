3efae72291069ef1da905d3c8f7a44f57ddd30ba
ely211 <gonzalesm405@gmail.com>
2017-05-10 14:14:15 -0500

    menu js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,8 @@
-var hugMenu = document.getElementById("hug-menu");
+var hugMenu = document.getElementById("btn-hug-menu");
 var menu = document.getElementById("menu");
+var labMenu = document.getElementById("lab-menu");
+
+labMenu.addEventListener("click", function(){
+  menu.style.display = "block"
+  // menu.classList.toggle("active");
+})
