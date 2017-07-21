3d36857c1bc2233710467a802c69f0312bccbd23
ely211 <gonzalesm405@gmail.com>
2017-07-21 02:14:20 -0500

    readme, css, js

--- README.md
+++ README.md
@@ -1,25 +1,65 @@
 # Freelancer
 
-Para trabajar en este proyecto, primero deberás clonarlo:
+Freelancer es un tema de Bootstrap.    
++ En ésta réplica el diseño se realiza con **flex-box**
++ Diseño responsive
 
-```sh
-git clone https://gitlab.com/laboratoria/freelancer.git
+---
+### Efecto scroll
++ Al bajar el scroll la barra de navegación es fija
++ Al bajar por cada sección cambia de color el item de la navegación que le corresponde
++ Al hacer click en cada uno de los item te dirige a  cada una de las secciones respectivamente
+
+##### _Desktop_
+>![Scroll effect in desktop](gif/freelancer-scroll.gif "Scroll effect in desktop")
+
+##### _Mobile_
+>![Scroll effect in mobile](gif/freelancer-scroll-mobile.gif "Scroll effect in mobile")
+
+----
+### Portafolio
++ Uso grid para las imágenes
++ Al hacer **hover** en las imágenes cambia el color de fondo
++ Al hacer **click** aparece un modal con la imágen y descripción del proyecto
++ Cada uno de los **modal** esta **creado dinámicamente** con **javascript**
+    * **Javascript**
+> ```
+var imageToShowModal = document.getElementsByClassName("image-to-show-modal");
+var modalContainer = document.getElementById("modal-container");
+var modalImage = document.getElementById("image-modal");
+var modalDescr = document.getElementById("proyect-description");
+for (var i = 0; i < imageToShowModal.length; i++){
+    imageToShowModal[i].addEventListener("click", function(event){
+        modalContainer.style.display = "block";
+        modalContainer.style.opacity = 1;
+        modalImage.src = event.currentTarget.nextElementSibling .src;
+    });
+}
 ```
 
-Luego, para poder trabajar en él y enlazarlo con nuestro repositorio, deberemos ingresar a la carpeta descargada y borrar el .git que nos trae por defecto (tener cuidado con el segundo comando, asegurarse de que esté bien escrito):
+##### _Desktop_
+>![Modal in desktop](gif/freelancer-modal-desktop.gif "Modal in desktop")
 
-```sh
-cd freelancer
-rm -rf .git
-```
+##### _Mobile_
+>![Modal in mobile](gif/freelancer-modal-mobile.gif "Modal in mobile")
 
-Por último, una vez creado tu repositorio en Github, simplemente inicializar Git y agregar tu remoto:
+---
+### Contacto
++ validación de formulario mediante HTML y javascript
+    * Al empezar a escribir aparece el **label** y si el campo está vacío desaparece
+    * Al hacer click en botón ![Form's send button](gif/freelancer-button-send.JPG "Form's send button")  el mensaje de error
+    * El input de **nombre** sólo acepta **letras**
+    * El input de **teléfono** sólo acepta números y está validaddo con regExp
+    
 
-```sh
-git init
-git remote add origin htttps://github.com/<usuario>/<nombre-repositorio>.git
-```
+##### _Desktop_
+>![Form in desktop](gif/freelancer-form-desktop.gif "Form in desktop")
 
-Con estos pasos ya puedes seguir tu workflow de siempre haciendo commits y pusheando a tu repositorio remoto :)
+##### _Mobile_
+>![Form in mobile](gif/freelancer-form-mobile.gif "Form in mobile")    
 
-`Recuerda`: Este proyecto consiste en replicar este [website](https://blackrockdigital.github.io/startbootstrap-freelancer/).
+---
+### Demo
+[Frelancer | flexbox](https://ely405.github.io/start-boostrap/?target=_blank)
+
+
--- js/app.js
+++ js/app.js
@@ -3,11 +3,9 @@ var menu = document.getElementById("menu");
 var labMenu = document.getElementById("lab-menu");
 
 labMenu.addEventListener("click", function(){
-  menu.style.display = "block"
-  // menu.classList.toggle("active");
+    menu.classList.toggle("d-block");
+    menu.classList.toggle("menu-none");
 })
-
-
 
 var profileSection = document.getElementById("profile-sec");
 var portfolioSection = document.getElementById("portfolio-Sec");
@@ -17,26 +15,103 @@ var aPortfolio = document.getElementById("a-port");
 var aAbout = document.getElementById("a-about");
 var aContact = document.getElementById("a-cont");
 
-function isInViewport(sectionInViewport, WithShade, WithoutShade) {
-  var top = sectionInViewport.offsetTop;
-  var left = sectionInViewport.offsetLeft;
-  var width = sectionInViewport.offsetWidth;
-  var height = sectionInViewport.offsetHeight;
-  if(top < (window.pageYOffset + window.innerHeight) && left < (window.pageXOffset + window.innerWidth) && (top + height) > window.pageYOffset && (left + width) > window.pageXOffset){
-    WithShade.className = "bc-green";
-    (WithShade.className == "bc-green")? WithoutShade.className = "bc-dark": "";
-  }else{
-    WithShade.className ="bc-dark";
-  }
+function isInViewport(sectionInViewport, WithShade, clShade, WithoutShade, clWithoutShade) {
+    var top = sectionInViewport.offsetTop;
+    var left = sectionInViewport.offsetLeft;
+    var width = sectionInViewport.offsetWidth;
+    var height = sectionInViewport.offsetHeight;
+    if(top < (window.pageYOffset + window.innerHeight) && left < (window.pageXOffset + window.innerWidth) && (top + height) > window.pageYOffset && (left + width) > window.pageXOffset){
+        WithShade.className = clShade;
+        (WithShade.className == clShade)? WithoutShade.className = clWithoutShade: "";
+    }else{
+        WithShade.className =clWithoutShade;
+    }
 }
 
 window.addEventListener("scroll", function(){
-  isInViewport(portfolioSection, aPortfolio, aAbout);
-  isInViewport(aboutSection, aAbout, aPortfolio);
-  isInViewport(contactSection, aContact, aAbout);
+    isInViewport(portfolioSection, aPortfolio, "bc-green", aAbout, "bc-black");
+    isInViewport(aboutSection, aAbout, "bc-green", aPortfolio, "bc-black");
+    isInViewport(contactSection, aContact, "bc-green", aAbout, "bc-black");
 
 });
 
 aPortfolio.addEventListener("click", function(){
-  portfolioSection.getBoundingClientRect().top = 90;
+    portfolioSection.getBoundingClientRect().top = 90;
 });
+
+//modal
+var imageToShowModal = document.getElementsByClassName("image-to-show-modal");
+var modalContainer = document.getElementById("modal-container");
+var modalImage = document.getElementById("image-modal");
+var modalDescr = document.getElementById("proyect-description");
+
+for (var i = 0; i < imageToShowModal.length; i++) {
+    imageToShowModal[i].addEventListener("click", function(event){
+        modalContainer.style.display = "block";
+        modalContainer.style.opacity = 1;
+        modalImage.src = event.currentTarget.nextElementSibling .src;
+    });
+}
+
+document.getElementById("close").addEventListener("click", function(){
+    modalContainer.style.display = "none";
+});
+document.getElementById("btn-close").addEventListener("click", function(){
+    modalContainer.style.display = "none";
+});
+
+var inpTextarea = document.getElementsByClassName("form-control");
+for(var i = 0; i< inpTextarea.length; i++){
+    inpTextarea[i].addEventListener("keyup", function(event){
+        (event.target.value != "")? event.target.previousElementSibling.classList.add("active"):
+        event.target.previousElementSibling.classList.remove("active");
+    })
+}
+
+function validateOnlyLetters(){
+    var keyCode = event.keyCode;
+    (keyCode >= 97 && keyCode <= 122 || keyCode >= 65 && keyCode <= 90 || keyCode == 39 || keyCode == 32 || keyCode == 13)? 
+        (event, event.target.nextElementSibling.innerText = ""):(event.preventDefault(), event.target.nextElementSibling.innerText = "*only enter letters*");
+}
+
+function validateOnlyNumber(){
+    console.log(event.keyCode);
+    (event.keyCode >= 48 && event.keyCode <= 57)? (event, event.target.nextElementSibling.innerText = ""):(event.preventDefault(), event.target.nextElementSibling.innerText = "*only enter number*");
+}
+
+function validateEmail(email){
+    var expEmail = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    event.target.nextElementSibling.innerText = (expEmail.test(email))? "": "*Not a valid email address*";
+}
+function validatePhone(phone){
+    console.log(phone);
+    var regCellPhone = /^[9]\d{7}$/;
+    event.target.nextElementSibling.innerText = (regCellPhone.test(phone))? "": "*Not a valid phone*";
+}
+
+var inpname  = document.getElementById("name");
+var inpPhone = document.getElementById("phone");
+var inpEmail = document.getElementById("email");
+var textMessage = document.getElementById("message");
+inpname.addEventListener("keypress", validateOnlyLetters);
+inpPhone.addEventListener("keypress", function(){
+    validateOnlyNumber();
+    validatePhone(inpPhone.value);
+});
+inpEmail.addEventListener("keypress", function(){
+    validateEmail(inpEmail.value);
+});
+textMessage.addEventListener("keypress", validateOnlyLetters);
+
+document.getElementById("btn-send").addEventListener("click", function(e){
+    /*e.preventDefault();*/
+    var error = document.getElementsByName("span-error");
+    console.log(inpPhone.value.length);
+    if(inpname.value == "" || inpPhone.value == ""|| inpEmail.value == "" || textMessage.value == ""){
+        error.forEach(function(elem){
+            if(elem.previousElementSibling.value == ""){
+                elem.innerHTML = elem.previousElementSibling.getAttribute("validation-message");
+            }
+        })  
+    }
+})
